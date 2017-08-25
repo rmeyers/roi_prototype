58c6205906702d3d5727113717cd7de60a9b5396
Ryan Meyers <ryan.d.meyers@gmail.com>
2017-08-25 13:00:14 -0400

    Change server_name to 0.0.0.0 to try to get it working on Heroku

--- server.py
+++ server.py
@@ -375,6 +375,6 @@ def getUserID(email):
 
 if __name__ == '__main__':
     app.secret_key = constants.SECRET_KEY
-    app.config['SERVER_NAME'] = 'https://sustainable-roi.herokuapp.com'
+    app.config['SERVER_NAME'] = '0.0.0.0'
     app.debug = True
     app.run(host=app.config['SERVER_NAME'], port=int(env.get('PORT', 5000)))
