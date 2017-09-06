b6b0d0bc88bf99424a622b75938eb5d82099ea01
vagrant <vagrant@vagrant.vm>
2017-09-06 22:00:09 +0000

    add prelim results content, error messages for buttons

--- static/js/app.js
+++ static/js/app.js
@@ -34,6 +34,47 @@ $(document).ready(function() {
     tr.find('#savings').html(savings);
 
   });
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 130
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  // Add smooth scrolling to all links
+  $(".delete-project").on('click', function(event) {
+
+    alert("Whoa there! No deleting this project quite yet, we're working on it though.");
+
+  });
+
+  $(".compare").on('click', function(event) {
+
+    alert("Sorry, unable to compare right now, but we're working on it!");
+
+  });
+
+  $(".export").on('click', function(event) {
+
+    alert("WORD!!");
+
+  });
 
 });
 
